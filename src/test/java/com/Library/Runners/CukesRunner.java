8c9fbcbd72ff35a57e4d683924ea43483d09c29e
“GULNURKIRCAER” <gulnur1984@yahoo.com>
2020-11-10 20:16:56 +0000

    new Proje

--- src/test/java/com/Library/Runners/CukesRunner.java
+++ src/test/java/com/Library/Runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/Library/StepDefinitions",
         dryRun = false,
-        tags="@wip"
+        tags="@login"
 )
 public class CukesRunner {
 }
